2ee583e7fd83c059b20a8f7d681ee3b02754d671
rmjkdev@163.com <ComeOn2021>
2021-09-27 10:17:59 +0800

    修改接口

--- HYSDoctorSDK/HYSDoctorSDK.framework/Headers/HYSDoctorSDK.h
+++ HYSDoctorSDK/HYSDoctorSDK.framework/Headers/HYSDoctorSDK.h
@@ -7,13 +7,28 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, HYSDoctorCallAgeType) {
+    HYSDoctorCallAgeTypeChild,
+    HYSDoctorCallAgeTypeAdult,
+};
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HYSDoctorSDK : NSObject
 
+@property (nonatomic, assign) BOOL isLogined;
+
 + (instancetype)shared;
 
-- (void)start;
+- (void)initWithProductId:(NSString *)productId enableLog:(BOOL)enableLog isDevelop:(BOOL)isDevelop;
+
+- (void)loginWithUserToken:(NSString *)token completion:(void (^ _Nonnull)(NSError * _Nullable))completion;
+
+- (void)startCallWithAgeType:(HYSDoctorCallAgeType)ageType;
+
+- (NSString *)getMedicListWithUserToken:(NSString *)token;
+
+- (void)logout:(void (^ _Nullable)(NSString * _Nullable))callback;
 
 @end
 
